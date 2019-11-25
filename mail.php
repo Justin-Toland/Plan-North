e648b5eb407dbf86109dfcb8f0a866b277ff6797
Toland <justin.toland@powereng.com>
2019-11-25 09:42:33 -0700

    Most recent progress. Need to optimize for mobile still and add animations.

--- mail.php
+++ mail.php
@@ -8,8 +8,8 @@
     $email = $_POST['email'];
     $message = $_POST['message'];
     $formcontent=" From: $name  \n Email: $email \n Message: $message";
-    $recipient = "malevolentseed@gmail.com";
-    $subject = "PLANNORTH Website Message";
+    $recipient = "info@plan-north.com";
+    $subject = "PLAN NORTH Website Message";
     $mailheader = "From: $email \r\n";
     mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 
